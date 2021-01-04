4b2c008673e8e88d2572213726249f4b557fa0fb
DxPHANTOMxD <49845089+DxPHANTOMxD@users.noreply.github.com>
2021-01-04 23:37:27 +0600

    Updates

---
+++ precache-manifest.d253aa22adc853c69fca29d65b85144f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c3def6b661f76887e31",
+    "url": "78.24.223.134/static/css/main.eb46a825.chunk.css"
+  },
+  {
+    "revision": "7c3def6b661f76887e31",
+    "url": "78.24.223.134/static/js/main.587a83ca.chunk.js"
+  },
+  {
+    "revision": "42e72504e4422f0ec4cf",
+    "url": "78.24.223.134/static/js/runtime~main.bb740505.js"
+  },
+  {
+    "revision": "5531f618a790bb64e466",
+    "url": "78.24.223.134/static/css/2.71cec654.chunk.css"
+  },
+  {
+    "revision": "5531f618a790bb64e466",
+    "url": "78.24.223.134/static/js/2.f1dc1d6c.chunk.js"
+  },
+  {
+    "revision": "60c290c0a4f8603cc5c0dd50ef947c70",
+    "url": "78.24.223.134/index.html"
+  }
+];
